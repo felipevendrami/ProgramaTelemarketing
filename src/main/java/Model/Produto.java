e9db659e3578217c98e00fdd9c550533aeb23ccf
Nicoly Cuch <nicolygabricuch@gmail.com>
2022-05-30 21:15:57 -0300

    Criação classe Produto e TipoProduto

--- src/main/java/Model/Produto.java
+++ src/main/java/Model/Produto.java
@@ -8,6 +8,52 @@ package Model;
  *
  * @author nicol
  */
-public class Produto {
+public class Produto extends Empresa{
+        
+    private String nomeProduto;
+    private double valorProduto;
+    private String descricaoProduto;
+    private Empresa empresa;
+
+    public Produto(String nomeProduto, double valorProduto, String descricaoProduto, Empresa empresa, String nome, Endereco endereco, Contato contato, String cnpj) {
+        super(nome, endereco, contato, cnpj);
+        this.nomeProduto = nomeProduto;
+        this.valorProduto = valorProduto;
+        this.descricaoProduto = descricaoProduto;
+        this.empresa = empresa;
+    }
+
+   
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setValorProduto(double valorProduto) {
+        this.valorProduto = valorProduto;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
     
 }
--- src/main/java/Model/TipoProduto.java
+++ src/main/java/Model/TipoProduto.java
@@ -4,10 +4,55 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author nicol
  */
 public class TipoProduto {
+    private String cor;
+    private double tamanho;
+    private Date garantia;
+    private Produto produto;
+
+    public TipoProduto(String cor, double tamanho, Date garantia, Produto produto) {
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.garantia = garantia;
+        this.produto = produto;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(double tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public Date getGarantia() {
+        return garantia;
+    }
+
+    public void setGarantia(Date garantia) {
+        this.garantia = garantia;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
     
 }
